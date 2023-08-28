c1c14d928450aadc19b4137e299303a58cb09281
natalie <natasha.glinkina2017@gmail.com>
2023-08-28 18:47:00 +0300

    добавлен цикл

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿string[] array1 = new string[4] {"11", "Natasha", "888", "hello"};
 string[] array2 = new string[array1.Length];
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}   
